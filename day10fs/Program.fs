297cd3a22074a5626e62f61af2ab20951e0d56f6
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2023-01-28 10:45:21 +0100

    day10 in fsharp

---
+++ day10fs/Program.fs
@@ -0,0 +1,49 @@
+open System
+
+type Instruction = | Noop | Addx of int
+exception UnknownInstruction of string
+
+let parseInstruction instruction =
+  match instruction with
+  | "noop" -> Noop
+  | s when s.StartsWith "addx" -> Addx(Array.get (s.Split " ") 1 |> int)
+  | s -> raise (UnknownInstruction(s))
+
+let scan collector append instructions =
+  let rec doScan collector cycle x instructions =
+    match instructions with
+    | [] -> collector
+    | Noop :: T -> doScan (collector |> append cycle x) (cycle+1) x T
+    | Addx v :: T -> doScan (collector |> append cycle x |> append (cycle+1) x) (cycle+2) (x+v) T
+  doScan collector 1 1 instructions
+
+let sample cycle x res =
+  if (cycle - 20) % 40 = 0 then
+    res + cycle * x
+  else
+    res
+
+let solutionPart1 instructions =
+  scan 0 sample instructions
+
+let draw cycle x res =
+  let hpos = (cycle - 1) % 40
+  res
+    + if x-1 <= hpos && hpos <= x+1 then "#" else "."
+    + if cycle % 40 = 0 then "\n" else ""
+  
+let solutionPart2 instructions =
+  scan "\n" draw instructions
+
+[<EntryPoint>]
+let main argv =
+    let input = (System.IO.File.ReadLines("input.txt")
+        |> Seq.map parseInstruction |> Seq.toList)
+
+    printfn "%s" (
+        match Environment.GetEnvironmentVariable("part") with
+        | null | "part1" -> solutionPart1 input |> string
+        | "part2" -> solutionPart2 input |> string
+        | env -> $"Unknown value {env}"
+    )
+    0 // return an integer exit code
